b96bfc4fce4d072bea0a6aaba31d2f205bc11242
Sean Wu <sean.wu@gmail.com>
2018-07-13 21:44:07 -0700

    changed memory size

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #### SELF PLAY
 EPISODES = 30
 MCTS_SIMS = 50
-MEMORY_SIZE = 30000
+MEMORY_SIZE = 500
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
