b29f0514aa6460ff3c1a96f68b777c1647033e41
Sean Wu <sean.wu@gmail.com>
2018-07-13 23:48:06 -0700

    made it shorter

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #### SELF PLAY
 EPISODES = 30
 MCTS_SIMS = 50
-MEMORY_SIZE = 500
+MEMORY_SIZE = 0
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
